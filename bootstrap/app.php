4258c52cfd73f830a57d708bf5f44d0e8956174f
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2025-11-27 23:19:39 +0700

    wil update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(except: [
             '/api/upload-video',
+            '/midtrans/callback'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
