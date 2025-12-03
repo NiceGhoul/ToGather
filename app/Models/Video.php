fd7a48f14064654625b237d28ecadf69b49267ee
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2025-12-03 21:31:55 +0700

    fix

---
+++ app/Models/Video.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Video extends Model
+{
+    use HasFactory;
+    protected $fillable = ['path', 'videoable_id', 'videoable_type'];
+    protected $appends = ['url'];
+    public function videoable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function getUrlAttribute()
+    {
+        return Storage::disk('minio')->url($this->path);
+    }
+}
