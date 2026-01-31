c88842e437ce32e04c37f707efdb29e9cdbdbd6e
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2026-01-31 17:45:43 +0700

    Nitification fix

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -302,6 +302,7 @@ class CampaignController extends Controller
             );
         }
         NotificationController::notifyUser(
+            auth()->id(),
             'campaign_created',
             'New Campaign Submitted',
             "New campaign '{$campaign->title}' has been submitted by {$campaign->user->nickname} and a draft has been made.",
